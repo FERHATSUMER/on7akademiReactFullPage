ce4baa6601e3e173beebe6abd62a5918f7e408c4
FERHATSUMER <sumerf39@gmail.com>
2023-09-20 22:14:07 +0300

    on7akademi

--- on7akademi/src/components/context/Context.js
+++ on7akademi/src/components/context/Context.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import { FcVideoCall } from "react-icons/fc";
+import { MdLiveTv } from "react-icons/md";
+import { AiOutlineProfile } from "react-icons/ai";
 import './context.css'
 const Context = () => {
   return (
-    <div>
-      <h1>Context</h1>
+    <div className="container">
+    <h1 className="contenth1">On7 Akademi</h1>
+    <div className="contentInfoContainer">
+      <div className="contentInfo">
+        <h2 className="infoTitle">Staj Eğtimi</h2>
+        <div className="infoButtonsContainer">
+          <button className="infoButton">
+            <AiOutlineProfile /> Dosyalar
+          </button>
+          <button className="infoButton">
+            <MdLiveTv /> Derse Katıl
+          </button>
+          <button className="infoButton">
+            <FcVideoCall /> Kayıtlı Dersler
+          </button>
+        </div>
+      </div>
+      <div className="contentInfo">
+        <h2 className="infoTitle">Staj Eğtimi2 </h2>
+        <div className="infoButtonsContainer">
+          <button className="infoButton">
+            <AiOutlineProfile /> Dosyalar
+          </button>
+          <button className="infoButton">
+            <MdLiveTv /> Derse Katıl
+          </button>
+          <button className="infoButton"s>
+            <FcVideoCall /> Kayıtlı Dersler
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
--- on7akademi/src/components/sidebar/Sidebar.js
+++ on7akademi/src/components/sidebar/Sidebar.js
@@ -1,34 +1,40 @@
+import React from "react";
 import { PiBooks, PiExamLight } from "react-icons/pi";
-import './sidebar.css'
+import { AiOutlineHome } from "react-icons/ai";
+import "./sidebar.css";
+
 const Sidebar = () => {
   return (
-    <div>
-    <div className="sidebarHeader">
-    <img
-      className="sidebarLogo"
-      src="https://media.licdn.com/dms/image/C560BAQG50mwENt9gSQ/company-logo_400_400/0/1632727910976?e=2147483647&v=beta&t=0w36fnGQ37PWTm253SLL0PVTr2ziC_mzcyO71DlLIDc"
-      alt="logo"
-    />
-    <h3 className="sidebarTitle">Kullanıcı Paneli</h3>
-  </div>
-  <div className="sidebarMenu">
-    <a href="/"><h3>ana sayfa</h3></a>  
-    <div className="sidebarMenuItem">
-      <PiBooks />
-      <a href="/card">
-        <h4> Dersler</h4>
-      </a>
+    <div className="sidebar-container">
+      <div className="sidebar-header">
+        <img
+          className="sidebar-logo"
+          src="https://media.licdn.com/dms/image/C560BAQG50mwENt9gSQ/company-logo_400_400/0/1632727910976?e=2147483647&v=beta&t=0w36fnGQ37PWTm253SLL0PVTr2ziC_mzcyO71DlLIDc"
+          alt="logo"
+        />
+        <h3 className="sidebar-title">Kullanıcı Paneli</h3>
+      </div>
+      <div className="sidebar-menu">
+        <a href="/">
+        <AiOutlineHome className="sidebar-icon"/> 
+          <h3>  Ana Sayfa</h3>
+        </a>
+        <div className="sidebar-menu-item">
+          <a href="/context">
+          <PiBooks className="sidebar-icon"/>
+            <h4> Dersler</h4>
+          </a>
+          <a href="/about">
+          <PiExamLight className="sidebar-icon"/>
+            <h4>
 
-      <PiExamLight />
-      <a href="/about">
-      <h4>Sınavlar</h4>
-      </a>
-     
-
+              Sınavlar
+            </h4>
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
