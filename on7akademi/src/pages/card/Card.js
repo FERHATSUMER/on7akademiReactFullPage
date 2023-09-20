a269de8b7464acce96a2e7679c12fcaed744db6c
FERHATSUMER <sumerf39@gmail.com>
2023-09-20 23:59:25 +0300

    on7akademi

--- on7akademi/src/pages/card/Card.js
+++ on7akademi/src/pages/card/Card.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import    './card.css'
-const Card = () => {
-  return (
-    <div>
-      <h1>Card</h1>
-    </div>
-  )
-}
+import "./card.css";
 
-export default Card
+const Card = ({ name, img, job, about, id }) => {
+  return (
+    <div className="card-container">
+      <div key={id} className="container">
+        <div className="card">
+          <img src={img} alt="resim" className="photo" />
+          <div className="intro">
+            <h1>{name}</h1>
+            <h3>{job}</h3>
+            <h4>{about}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
